3ec64484d2ab67bda36c5ee3052cb672b9b850dd
ricardo-limaco <ricardolimaco.05@gmail.com>
2021-11-03 16:53:31 +0100

    Sidebar

---
+++ Milestone 1/js/script.js
@@ -0,0 +1,100 @@
+Vue.config.devtools = true;
+
+new Vue ({
+  el: "#root",
+  data:{
+    contacts: [
+      //Michele
+      {
+        name: "Michele",
+        avatar: "img/avatar_1.jpg",
+        visible: true,
+        messages: [
+        //singolo messaggio
+          {
+            date: "10/01/2020 15:30:55",
+            text: "Hai portato a sapasso il cane?",
+            status: "sent",
+          },
+          {
+            date: "10/01/2020 15:50:00",
+            text: "Ricordati di dargli da mangiare",
+            status: "sent",
+          },
+          {
+            date: "10/01/2020 16:15:22",
+            text: "Tutto fatto!",
+            status: "recived",
+          },
+        ],
+      },
+      //Fabio
+      {
+        name: "Fabio",
+        avatar: "img/avatar_2.jpg",
+        visible: true,
+        messages: [
+        //singolo messaggio
+          {
+            date: "20/03/2020 16:30:00",
+            text: "Ciao come stai?",
+            status: "sent",
+          },
+          {
+            date: "20/03/2020 16:30:55",
+            text: "Bene grazie! Stasera ci vediamo?",
+            status: "recived",
+          },
+          {
+            date: "20/03/2020 16:35:00",
+            text: "Mi piacerebbe ma devo andare a fare la spesa.",
+            status: "sent",
+          },
+        ],
+      },
+      //Samuele
+      {
+        name: "Samuele",
+        avatar: "img/avatar_3.jpg",
+        visible: true,
+        messages: [
+        //singolo messaggio
+          {
+            date: "28/03/2020 10:10:40",
+            text: "La Marianna va in campagna",
+            status: "recived",
+          },
+          {
+            date: "28/03/2020 10:20:10",
+            text: "Sicuro di non aver sbagliato chat?",
+            status: "sent",
+          },
+          {
+            date: "28/03/2020 16:15:22",
+            text: "Ah scusa!",
+            status: "recived",
+          },
+        ],
+      },
+      //Luisa
+      {
+        name: "Luisa",
+        avatar: "img/avatar_4.jpg",
+        visible: true,
+        messages: [
+        //singolo messaggio
+          {
+            date: "10/01/2020 15:30:55",
+            text: "Lo sai che ha aperto una nuova pizzeria?",
+            status: "sent",
+          },
+          {
+            date: "10/01/2020 15:50:00",
+            text: "Si, ma preferirei andare al cinema",
+            status: "recived",
+          },
+        ],
+      },
+    ],
+  },
+})
